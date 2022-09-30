e91c3d21c34d000137e5d312d898d1a1a6024b64
Shinde kudasai ❤〜 <changlovemom@gmail.com>
2022-09-30 13:06:56 +0700

    feat: add min-height

--- src/Interface/Header.jsx
+++ src/Interface/Header.jsx
@@ -1,81 +1,66 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 // ` `
 // background: linear-gradient(to left, #ee9ca7, #ffdde1);
 // background-image: url("bg1.png");
 export const HeaderContainer = styled.div`
-    height: 1000px;
-    max-width: 1640px;
-    width: 100%;
-    border-left: 1px solid black;
-    justify-content:center;
-    align-items:center;
-    background-color : #f5f2eb;
-    padding: 12px 120px 80px 80px;
-    padding-left: 100px;
-`
+  min-height: 1000px;
+  max-width: 1640px;
+  width: 100%;
+  border-left: 1px solid black;
+  justify-content: center;
+  align-items: center;
+  background-color: #f5f2eb;
+  padding: 12px 120px 80px 80px;
+  padding-left: 100px;
+`;
 
 export const Container1 = styled.div`
-    margin-top: 25px;
-    width:350px;
-    text-align: center;
+  margin-top: 25px;
+  width: 350px;
+  text-align: center;
 
-    color: black;
+  color: black;
 
-    transition: 0.5s ease-out;
-
-`
+  transition: 0.5s ease-out;
+`;
 
 export const Container2 = styled.div`
-    width:80%;
-    border-radius: 5px;
-
-`
+  width: 80%;
+  border-radius: 5px;
+`;
 
 export const Container3 = styled.div`
-    padding-left : 30px;
-    padding-right : 30px;
-    margin-bottom: 25px;
-    margin-top: 10px;
-    width:40%;
-    border-radius: 5px;
-    border : 1px solid black;
+  padding-left: 30px;
+  padding-right: 30px;
+  margin-bottom: 25px;
+  margin-top: 10px;
+  width: 40%;
+  border-radius: 5px;
+  border: 1px solid black;
 
-    transition: 0.1s ease-out;
-    &:hover{
-        
-        
-    }
-
-
-`
+  transition: 0.1s ease-out;
+  &:hover {
+  }
+`;
 export const Container4 = styled.div`
-    padding-left : 30px;
-    padding-right : 30px;
-    margin-top: 10px;
-    width:40%;
-    margin-bottom: 25px;
-    border-radius: 5px;
-    border : 1px solid black;
-
-
-`
+  padding-left: 30px;
+  padding-right: 30px;
+  margin-top: 10px;
+  width: 40%;
+  margin-bottom: 25px;
+  border-radius: 5px;
+  border: 1px solid black;
+`;
 export const Container5 = styled.div`
-    padding-left : 30px;
-    padding-right : 30px;
-    margin-bottom: 25px;
-    margin-top: 10px;
-    width:80%;
-    margin-bottom: 10px;
+  padding-left: 30px;
+  padding-right: 30px;
+  margin-bottom: 25px;
+  margin-top: 10px;
+  width: 80%;
+  margin-bottom: 10px;
 
-    border-radius: 5px;
-    border : 1px solid black;
+  border-radius: 5px;
+  border: 1px solid black;
+`;
 
-
-`
-
-export const Text1 = styled.p`
-
-
-`
-
-
+export const Text1 = styled.p``;
